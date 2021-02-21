3aa5d7522f4818826753d31f11e8bad52d9fcf19
Duilio Palacios <duilio@styde.net>
2021-02-21 19:16:21 +0000

    Fake API and static expected result

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('external-videos/{limit?}', function ($take = 20) {
+    return \App\Vendor\FakeVideoLibrary::make()->latest($take);
+});
