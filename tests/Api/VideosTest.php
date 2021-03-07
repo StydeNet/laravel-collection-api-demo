6c3c700f783021f642a21cd0e81085a1d65f3562
Duilio Palacios <duilio@styde.net>
2021-03-07 20:13:49 +0000

    Add test

---
+++ tests/Api/VideosTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Api;
+
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class VideosTest extends TestCase
+{
+    /** @test */
+    function gets_videos_grouped_by_playlist()
+    {
+        Http::fake([
+            url('api/external-videos') => Http::response($this->getJsonData('external-videos'), 200, [])
+        ]);
+
+        $this->get('api/videos')
+            ->assertJson($this->getJsonData('videos'));
+    }
+
+    protected function getJsonData($filename)
+    {
+        return json_decode(file_get_contents(__DIR__."/data/{$filename}.json"), JSON_OBJECT_AS_ARRAY);
+    }
+}
