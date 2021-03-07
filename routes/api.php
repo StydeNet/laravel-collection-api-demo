89ba2d0a0c5ca6695488461491e334645562d061
Duilio Palacios <duilio@styde.net>
2021-03-07 11:33:19 +0000

    Add videos route

--- routes/api.php
+++ routes/api.php
@@ -5,19 +5,62 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API interna (de nuestro proyecto)
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| Éste es un ejemplo de una API interna que se comunicará con el API de
+| otro proyecto para obtener datos, procesarlos y retornarlos en otro
+| formato, por ejemplo, para ser usados por el frontend de la app.
 |
 */
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('videos', function () {
+    $apiUrl = url('api/external-videos'); // https://external-videos.com/videos
+
+    $response = Http::get($apiUrl);
+
+    $data = $response->json()['data'];
+
+    return collect($data)
+        ->map(function ($row) {
+            return [
+                'title' => $row['title'],
+                'description' => $row['description'],
+                'length' => $row['length'],
+                'score' => $row['likes'] + $row['views'],
+                'channel' => $row['channel']['name'],
+                'author' => $row['channel']['author']['name'],
+                'tags' => collect(explode(',', $row['tags']))
+                    ->map(function ($tag) {
+                        return ucfirst($tag);
+                    })
+                    ->all(),
+                'playlist' => $row['playlist'],
+            ];
+        })
+        ->groupBy('playlist')
+        ->map(function ($videos, $playlistName) {
+            return [
+                'name' => Str::title($playlistName),
+                'length' => $videos->sum('length'),
+                'videos' => $videos
+            ];
+        });
+});
+
+/*
+|--------------------------------------------------------------------------
+| Simulación de un API externa (de otro proyecto)
+|--------------------------------------------------------------------------
+|
+| Estas rutas y este código estarían en un proyecto aparte; al cual,
+| quizá, no tendrías acceso ni a su código ni a su base de datos;
+| por lo tanto deberías interactuar con éste través del API.
+|
+*/
 Route::get('external-videos/{limit?}', function ($take = 20) {
     return \App\Vendor\FakeVideoLibrary::make()->latest($take);
 });
