b76049196349ab6078417851b77c95c08f5c5b32
Duilio Palacios <duilio@styde.net>
2021-03-24 15:23:37 +0000

    Use data providers

--- tests/Api/GetPlaylistsWithVideosTest.php
+++ tests/Api/GetPlaylistsWithVideosTest.php
@@ -7,47 +7,58 @@ use App\Api\Entities\Playlist;
 use App\Api\Entities\Video;
 use App\Api\Fake\GetFakePlaylistsWithVideos;
 use App\Api\GetExternalPlaylistsWithVideos;
+use Closure;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class GetPlaylistsWithVideosTest extends TestCase
 {
-    /** @test */
-    function get_playlists_with_videos()
+    /**
+     * @test
+     * @dataProvider gatewayProviders
+     */
+    function get_playlists_with_videos(Closure $gatewayFactory)
     {
-        Http::fake([
-            url('api/external-videos') => Http::response($this->getJsonData('external-videos'), 200, [])
-        ]);
+        $gateway = $gatewayFactory();
 
-        $gateways = [
-            new GetExternalPlaylistsWithVideos,
-            new GetFakePlaylistsWithVideos,
-        ];
+        $this->assertInstanceOf(GetPlaylistsWithVideos::class, $gateway);
 
-        foreach ($gateways as $gateway) {
-            $this->assertInstanceOf(GetPlaylistsWithVideos::class, $gateway);
+        $result = $gateway->get();
 
-            $result = $gateway->get();
+        $this->assertInstanceOf(Collection::class, $result);
 
-            $this->assertInstanceOf(Collection::class, $result);
+        $result->each(function ($playlist) {
+            $this->assertInstanceOf(Playlist::class, $playlist);
 
-            $result->each(function ($playlist) {
-                $this->assertInstanceOf(Playlist::class, $playlist);
+            $this->assertInstanceOf(Collection::class, $playlist->getVideos());
 
-                $this->assertInstanceOf(Collection::class, $playlist->getVideos());
-
-                $playlist->getVideos()->each(function ($video) {
-                    $this->assertInstanceOf(Video::class, $video);
-                });
+            $playlist->getVideos()->each(function ($video) {
+                $this->assertInstanceOf(Video::class, $video);
             });
+        });
 
-            $this->assertSame($this->getJsonData('videos'), $result->toArray());
-        }
+        $this->assertSame($this->getJsonData('videos'), $result->toArray());
     }
 
     protected function getJsonData($filename)
     {
         return json_decode(file_get_contents(__DIR__."/data/{$filename}.json"), JSON_OBJECT_AS_ARRAY);
+    }
+
+    public function gatewayProviders()
+    {
+        return [
+            'External Gateway' => [function () {
+                Http::fake([
+                    url('api/external-videos') => Http::response($this->getJsonData('external-videos'), 200, [])
+                ]);
+
+                return new GetExternalPlaylistsWithVideos;
+            }],
+            'Fake Gateway' => [function () {
+                return new GetFakePlaylistsWithVideos;
+            }],
+        ];
     }
 }
